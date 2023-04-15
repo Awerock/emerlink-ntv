77754d42a955de1cff93033fa970cc2bc258d180
Awerock <cigan-yashka@yandex.ru>
2023-04-15 18:23:43 +0300

    add: player

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,28 @@ setInterval(function () {
     var clock = document.getElementById("clock");
     clock.innerHTML = now.toLocaleTimeString();
 }, 1000);
+
+/* document.addEventListener("DOMContentLoaded", function() {
+    const cardClick = document.getElementsByClassName("card");
+    for (i = 0; i < cardClick.length; i++) {
+        cardClick[i].addEventListener("click", function() {
+            if (!this.classList.contains("card_active")) {
+                this.classList.add("card_active");
+            } else {
+                this.classList.remove("card_active");
+            }
+    });
+    }
+}) */
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("on-of").addEventListener("click", function() {
+        if (!document.querySelector(".main").classList.contains("card_active")) {
+            document.querySelector(".main").classList.add("card_active");
+            document.getElementById("on-of").classList.add("card_active");
+        } else {
+            document.querySelector(".main").classList.remove("card_active");
+            document.getElementById("on-of").classList.remove("card_active");
+        }
+    });
+})
